72b0fac38ca96e240632234bc1768b7bb1754fb2
Marco <marco.boutchelski@gmail.com>
2024-12-26 12:58:40 +0000

    clear project

--- test/index.js
+++
@@ -1,3 +0,0 @@
-// neither .mocharc.js nor karma options to require seem to work
-import './prepare.js'; // eslint-disable-line import/extensions
-import './index.jsx';
--- test/prepare.js
+++
@@ -1,6 +0,0 @@
-import chai from 'chai';
-import sinon from 'sinon-chai';
-import dirty from 'dirty-chai';
-
-chai.use(sinon);
-chai.use(dirty);
