9d235db4c069c2704d64a1889d4b7b31afb63e35
Marco <marco.boutchelski@gmail.com>
2024-12-28 00:24:36 +0000

    create examples

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RefObject, useRef } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import useScrollOnDrag from 'react-scroll-ondrag';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Container = styled.div`
+  display: inline-block;
+  width: 1000px;
+  height: 250px;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  border: 1px solid #000;
+  padding: 0 5px;
+  white-space: nowrap;
+`;
+
+const Box = styled.div`
+  display: inline-block;
+  height: 300px;
+  margin: 5px 10px;
+  width: 250px;
+  background: linear-gradient(red, yellow);
+`;
+
+const ScrollableBox = ({
+  runScroll,
+}: {
+  runScroll?: (
+    containerRef: RefObject<HTMLDivElement>,
+  ) => (args: { dx: number; dy: number }) => void;
+}) => {
+  const containerRef = useRef(null);
+  const { events } = useScrollOnDrag(containerRef, {
+    runScroll: runScroll && runScroll(containerRef),
+  });
 
   return (
+    <Container {...events} ref={containerRef}>
+      {[...Array.from({ length: 30 }).keys()].map((i) => (
+        <Box key={i} />
+      ))}
+    </Container>
+  );
+};
+
+ScrollableBox.propTypes = {
+  runScroll: PropTypes.func,
+};
+
+ScrollableBox.defaultProps = {
+  runScroll: undefined,
+};
+
+
+function App() {
+  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Default runScroll, scrolls both x and y directions:</div>
+      <ScrollableBox />
+      <div>Scrolls only x direction at 5 times the normal speed:</div>
+      <ScrollableBox
+        runScroll={(containerRef) =>
+          ({ dx }: { dx: number }) => {
+            if (containerRef.current) {
+              containerRef.current.scrollLeft += dx * 5;
+            }
+          }}
+      />
     </>
   )
 }
